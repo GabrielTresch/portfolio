4be65bc89c2f5944717bf234084cd5dd8f4f63dc
GabrielTresch <gabriel_tresch@outlook.com>
2019-09-19 22:26:41 +0200

    realisation cards modifs

--- src/components/RealisationCard.js
+++ src/components/RealisationCard.js
@@ -25,6 +25,13 @@ const useStyles = makeStyles({
     margin: '0 auto 0 0',
     fontSize: '12px',
   },
+  competences: {
+    background: 'black',
+    color: 'white',
+    fontSize: '12px',
+    padding: '2px 5px',
+    margin: 'auto 5px',
+  },
 });
 
 const RealisationCard = ({
@@ -32,18 +39,20 @@ const RealisationCard = ({
 }) => {
   const classes = useStyles();
   return (
-    <a href={link} key={id} className={classes.container}>
-      <img src={PortfolioImg} alt="portfolio" className={classes.img} />
+    <div key={id} className={classes.container}>
+      <a href={link}>
+        <img src={PortfolioImg} alt="portfolio" className={classes.img} />
+      </a>
       <div className={classes.content}>
         <h2 className={classes.title}>{title}</h2>
         <h3 className={classes.date}>{date}</h3>
         <p>{description}</p>
         {competences.split(',').map((value, index) => (
           // eslint-disable-next-line react/no-array-index-key
-          <span key={index}>{value}</span>
+          <span key={index} className={classes.competences}>{value}</span>
         ))}
       </div>
-    </a>
+    </div>
   );
 };
 

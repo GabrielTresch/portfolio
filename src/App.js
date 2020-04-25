fb9efad87c71e3f730d1417d8946fd34bcdba209
GabrielTresch <gabriel_tresch@outlook.com>
2020-04-25 11:40:05 +0200

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import { Router } from 'react-router-dom';
 import { createMuiTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -17,6 +18,14 @@ const theme = createMuiTheme({
 });
 
 const history = createBrowserHistory();
+
+ReactGA.initialize('UA-120472258-3');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'c',
